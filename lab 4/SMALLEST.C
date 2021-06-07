c0e0c99307d7a14c0d7a1338706e531241012864
AnvitaBMS <83911013+AnvitaBMS@users.noreply.github.com>
2021-06-07 11:33:11 +0530

    Add files via upload

---
+++ lab 4/SMALLEST.C
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+ int num1, num2, num3;
+ printf("\nenter three numbers: ");
+ scanf("%d %d %d", &num1, &num2, &num3);
+ if(num1<num2 && num1<num3) {
+  printf("\n%d is the smallest of the three numbers", num1);
+ }
+ else if(num2<num3) {
+  printf("\n%d is the smallest of the three numbers", num2);
+ }
+ else {
+  printf("\n%d is the smallest of the three numbers", num3);
+ }
+ return (0);
+}
