8c3228120981aa162b4682a1c9a62e58e4ad9854
AnvitaBMS <83911013+AnvitaBMS@users.noreply.github.com>
2021-06-14 10:02:23 +0530

    Add files via upload

---
+++ Lab 5/CONSONAN.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+ char ch;
+ printf("\nenter any alphabet: ");
+ scanf("%c", &ch);
+ switch(ch)
+ {
+  case 'a':
+  case 'e':
+  case 'i':
+  case 'o':
+  case 'u':
+  case 'A':
+  case 'E':
+  case 'I':
+  case 'O':
+  case 'U':
+   printf("vowel");
+   break;
+  default:
+   printf("consonant");
+  getch();
+ }
+ return (0);
+}
